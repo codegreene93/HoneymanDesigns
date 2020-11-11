42bd828ee95cfbc13852847dbca623196f6870b6
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2020-11-11 19:49:34 +0000

    redux

---
+++ backend/data.js
@@ -0,0 +1,37 @@
+export default {
+  products: [
+      {
+      _id: '3',
+      name: 'Monkey Light',
+      category: 'lights',
+      image: '/images/monkey.jpg',
+      price: '80',
+      brand: 'Acuity',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 8,
+      },
+      {
+      _id: '2',
+      name: 'Elephant Lamp',
+      category: 'lights',
+      image: '/images/monkey.jpg',
+      price: '80',
+      brand: 'Acuity',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 9,
+      },
+      {
+      _id: '3',
+      name: 'Decorative Lamp',
+      category: 'lights',
+      image: '/images/monkey.jpg',
+      price: '80',
+      brand: 'Acuity',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 11,
+      }
+]
+}
